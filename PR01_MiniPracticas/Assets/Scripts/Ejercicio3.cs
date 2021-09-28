095eecce417176be8c5db99814856938bd4fc423
Maik <ia4lledo@gmail.com>
2021-09-28 11:04:24 +0200

    Ejercicios 3 y 4 a medias

--- PR01_MiniPracticas/Assets/Scripts/Ejercicio3.cs
+++ PR01_MiniPracticas/Assets/Scripts/Ejercicio3.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Ejercicio3 : MonoBehaviour
 {
+    public Text timer;
+    private float intervalo = 1.0f;
+    private int[] timeNums = new int[3] {0, 0, 0};
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,22 @@ public class Ejercicio3 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        intervalo -= Time.deltaTime;
+        if (intervalo < 0)
+        {
+            intervalo = 1;
+            timeNums[0]++;
+            if (timeNums[0] > 59)
+            {
+                timeNums[0] = 0;
+                timeNums[1]++;
+                if (timeNums[1] > 59)
+                {
+                    timeNums[1] = 0;
+                    timeNums[2]++;
+                }
+            }
+            print(timeNums[2] + ":" + timeNums[1] + ":" + timeNums[0]);
+        }
     }
 }
--- PR01_MiniPracticas/Assets/Scripts/Ejercicio4.cs
+++ PR01_MiniPracticas/Assets/Scripts/Ejercicio4.cs
@@ -4,22 +4,36 @@ using UnityEngine;
 
 public class Ejercicio4 : MonoBehaviour
 {
+    public float speed = 0.1f;
+    private Vector3 forward = new Vector3(0f,0f,0.1f);
+    private Vector3 back = new Vector3(0f,0f,-0.1f);
+    private Vector3 left = new Vector3(-0.1f,0f,0f);
+    private Vector3 right = new Vector3(0.1f,0f,0f);
     // Start is called before the first frame update
     void Start()
     {
-        bool goForward;
-        bool goBack;
-        bool goLeft;
-        bool goRight;
+        
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("w"))
+        if (Input.GetKey(KeyCode.W))
         {
-
+            transform.position += forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += back * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += right * speed * Time.deltaTime;
         }
     }
 }
